25b8f238b84aefbb2991819a4c667fb4bb695138
陈皓楠 <1274634159@qq.com>
2023-05-10 18:00:19 +0800

    add health

--- api/init.go
+++ api/init.go
@@ -1,10 +1,15 @@
 package api
 
 import (
+	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+var started time.Time
 
 // Init 初始化服务
 func Init() {
@@ -17,7 +22,27 @@ func Init() {
 	// 其他需要初始化的sdk和internal包在这个位置完成
 
 	// 路由初始化
+	started = time.Now()
 	r := gin.Default()
+
+	r.GET("/health", health)
+
 	initRouter(r)
-	log.Fatal(r.Run(":8081"))
+	log.Fatal(r.Run(":9093"))
+}
+
+func health(ctx *gin.Context) {
+	w := ctx.Writer
+	duration := time.Now().Sub(started)
+	if duration.Seconds() > 20 {
+		log.Println("healthz 健康检查失败")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(fmt.Sprintf("error: #{duration.Seconds()}")))
+		return
+	} else {
+		log.Println("Healthz 健康检查成功")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	}
+
 }
